6a04a58d2cd9409581b87ad814d12e27889fc179
tomasantolaria <109923421+tomasantolaria@users.noreply.github.com>
2022-08-30 21:03:37 -0300

    Add files via upload

---
+++ Ejercicio modulo 5.js
@@ -0,0 +1,8 @@
+//Ejercicio modulo 5
+let altura_centimetros = 180;
+let altura_metros = 1.78;
+let peso = 75.6;
+let alt_redondeada = Math.round(altura_centimetros);
+let peso_redondeado = Math.floor(peso);
+let maximo = "El maximo valor que se puede obtener en Javascript + 1 = " + Number.MAX_VALUE;
+
